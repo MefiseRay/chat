7af4ef24220ff7a557222d95329df2d664e11c2a
Max Zaitsev <zaytsev.max90@gmail.com>
2022-09-20 15:00:54 +0300

    Абстрактный класс для апи

---
+++ src/api/BaseAPI.ts
@@ -0,0 +1,22 @@
+import HTTPTransport from '../utils/HTTPTransport';
+
+export default abstract class BaseAPI {
+  protected http: HTTPTransport;
+
+  protected constructor(endpoint: string) {
+    this.http = new HTTPTransport(endpoint);
+  }
+}
+
+export interface BaseCreateAPI {
+  create(data: unknown): Promise<unknown>;
+}
+export interface BaseReadAPI {
+  read(identifier?: string): Promise<unknown>;
+}
+export interface BaseUpdateAPI {
+  update(identifier: string, data: unknown): Promise<unknown>;
+}
+export interface BaseDeleteAPI {
+  delete(identifier: string): Promise<unknown>;
+}
